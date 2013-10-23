419f56bee0ed1abd81d5b1ce9f6ff0986b1a0855
xurxosf <xurxosf@gmail.com>
2013-10-23 22:50:55 +0200

    Añado ficheros

---
+++ Aspects/TimeTraceAspect.cs
@@ -0,0 +1,39 @@
+﻿using PostSharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOPConPostsharp.Aspects
+{
+    [Serializable] 
+    public sealed class TimeTraceAspect : OnMethodBoundaryAspect
+    {
+        private readonly string _name;
+
+        [NonSerialized]
+        System.Diagnostics.Stopwatch watch;
+
+        public TimeTraceAspect(string name)
+        {
+            _name = name;
+        }
+
+        public TimeTraceAspect()
+        {
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            Console.WriteLine(string.Format("Inicio Metodo {0}", _name ?? args.Method.Name));
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            watch.Stop();
+            Console.WriteLine(string.Format("Fin Metodo {0}. Tiempo {1}", _name ?? args.Method.Name, watch.ElapsedMilliseconds));
+        } 
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using AOPConPostsharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOPConPostsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Console.Read();
+        }
+
+        [TimeTraceAspect("uno")]
+        private static void Method1()
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        [TimeTraceAspect]
+        private static void Method2()
+        {
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
